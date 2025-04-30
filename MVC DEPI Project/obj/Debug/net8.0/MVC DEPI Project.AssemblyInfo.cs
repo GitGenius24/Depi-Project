c733fe6302bfd732b20a04b266209f61b7f9ad4a
Noureldein148 <1803671@eng.asu.edu.eg>
2025-04-30 18:02:24 +0300

    Updating all Views

--- MVC DEPI Project/obj/Debug/net8.0/MVC DEPI Project.AssemblyInfo.cs
+++ MVC DEPI Project/obj/Debug/net8.0/MVC DEPI Project.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC DEPI Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617c04bafa02ca4fc206a952ccf2596fbd948414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcf48a1f2d5b13025cd199c6c00a84295409043")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC DEPI Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC DEPI Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
